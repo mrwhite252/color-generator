d535ee8cfd91bc13fbf39d0a00dc51fa9589772d
DavidBai <baiminghan@gmail.com>
2021-11-02 12:35:52 +1300

    demo function of generate hex value

---
+++ app.js
@@ -0,0 +1,15 @@
+// global selections and variables
+
+// functions
+
+function generateHex() {
+  const letters = "#0123456789ABCDEF";
+  let hash = "#";
+  for (let i = 0; i < 6; i++) {
+    hash += letters[Math.floor(Math.random() * 16)];
+  }
+  return hash;
+}
+
+let randomHex = generateHex();
+console.log(randomHex);
