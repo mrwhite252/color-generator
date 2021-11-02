d5f45cfb897974957ccdbea67958991d678ebc3d
DavidBai <baiminghan@gmail.com>
2021-11-02 13:04:31 +1300

    add check contrast function

--- app.js
+++ app.js
@@ -36,7 +36,20 @@ function randomColors() {
     // add the random color to the background
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
+
+    // check for contrast
+
+    checkTextContrast(randomColor, hexText);
   });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
 }
 
 randomColors();
